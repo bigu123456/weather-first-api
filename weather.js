f0b3efca398ad9e6433694a5a4957d131762b739
bigu123456 <bigyanacharya905@gmail.com>
2023-07-09 09:29:40 +0545

    second  weather app with change in css

--- weather.js
+++ weather.js
@@ -1,6 +1,5 @@
 // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
 const apiKey = '6498b3255d6e916bd41bd3d9f1d677ee';
-
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid=${apiKey}&units=metric`;
 
 const weatherContainer = document.getElementById('weather');
@@ -10,72 +9,73 @@ const cityInput = document.getElementById('city-input');
 const cityNameElement = document.getElementById('city-name');
 const dateElement = document.getElementById('date');
 
-searchButton.addEventListener('mouseout', () => {
+searchButton.addEventListener('mouseout', async () => {
   const city = cityInput.value.trim();
   if (city) {
-    getWeather(city);
+    await getWeather(city);
     cityInput.value = '';
   }
 });
 
-getWeather('Guildford');
+getWeather('Barcelona');
 
-function getWeather(city) {
+async function getWeather(city) {
   const url = apiUrl.replace('{CITY_NAME}', city);
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const cityName = `${data.name}, ${data.sys.country}`;
-      const currentDate = new Date().toLocaleDateString('en-US', { dateStyle: 'long' });
-      const weatherCondition = data.weather[0].description;
-      const iconCode = data.weather[0].icon;
-      const temperature = data.main.temp;
-      const pressure = data.main.pressure;
-      const windSpeed = data.wind.speed;
-      const humidity = data.main.humidity;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-      const weatherHtml = `
-        <div class="weather-parameter">
-          <i class="wi wi-${getWeatherIcon(iconCode)} icon"></i>
-          <div class="value">${weatherCondition}</div>
-          <div class="label">Weather Condition</div>
-        </div>
-        <div class="weather-parameter">
-          <i class="wi wi-thermometer icon"></i>
-          <div class="value">${temperature}°C</div>
-          <div class="label">Temperature</div>
-        </div>
-        <div class="weather-parameter">
-          <i class="wi wi-barometer icon"></i>
-          <div class="value">${pressure} hPa</div>
-          <div class="label">Pressure</div>
-        </div>
-        <div class="weather-parameter">
-          <i class="wi wi-strong-wind icon"></i>
-          <div class="value">${windSpeed} m/s</div>
-          <div class="label">Wind Speed</div>
-        </div>
-        <div class="weather-parameter">
-          <i class="wi wi-humidity icon"></i>
-          <div class="value">${humidity}%</div>
-          <div class="label">Humidity</div>
-        </div>
-      `;
+    const cityName = `${data.name}, ${data.sys.country}`;
+    const currentDate = new Date().toLocaleDateString('nepali', { dateStyle: 'short' });
+    const weatherCondition = data.weather[0].description;
+    const iconCode = data.weather[0].icon;
+    const temperature = data.main.temp;
+    const pressure = data.main.pressure;
+    const windSpeed = data.wind.speed;
+    const humidity = data.main.humidity;
 
-      weatherContainer.innerHTML = weatherHtml;
+    const weatherHtml = `
+      <div class="weather-parameter">
+        <i class="wi wi-${getWeatherIcon(iconCode)} icon"></i>
+        <div class="value">${weatherCondition}</div>
+        <div class="label">Weather Condition</div>
+      </div>
+      <div class="weather-parameter">
+        <i class="wi wi-thermometer icon"></i>
+        <div class="value">${temperature}°C</div>
+        <div class="label">Temperature</div>
+      </div>
+      <div class="weather-parameter">
+        <i class="wi wi-barometer icon"></i>
+        <div class="value">${pressure} hPa</div>
+        <div class="label">Pressure</div>
+      </div>
+      <div class="weather-parameter">
+        <i class="wi wi-strong-wind icon"></i>
+        <div class="value">${windSpeed} m/s</div>
+        <div class="label">Wind Speed</div>
+      </div>
+      <div class="weather-parameter">
+        <i class="wi wi-humidity icon"></i>
+        <div class="value">${humidity}%</div>
+        <div class="label">Humidity</div>
+      </div>
+    `;
 
-      const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-      weatherIconContainer.innerHTML = `<img src="${iconUrl}" alt="Weather Icon">`;
+    weatherContainer.innerHTML = weatherHtml;
 
-      cityNameElement.textContent = cityName;
-      dateElement.textContent = currentDate;
-    })
-    .catch(error => {
-      console.log('Error:', error);
-      weatherContainer.textContent = 'Failed to fetch weather data.';
-      weatherIconContainer.innerHTML = '';
-    });
+    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+    weatherIconContainer.innerHTML = `<img src="${iconUrl}" alt="Weather Icon">`;
+
+    cityNameElement.textContent = cityName;
+    dateElement.textContent = currentDate;
+
+  } catch (error) {
+    console.log('Error:', error);
+    weatherContainer.textContent = 'Failed to fetch weather data.';
+    weatherIconContainer.innerHTML = '';
+  }
 }
 
 function getWeatherIcon(iconCode) {
